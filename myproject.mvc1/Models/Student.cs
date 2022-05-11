6d075abde1320ec1217f5b2d4ef17294f222772a
sai-sumanth639 <65843726+sai-sumanth639@users.noreply.github.com>
2022-05-11 10:50:27 +0530

    Add project files.

---
+++ myproject.mvc1/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentMarkAutomation.Models
+{
+    public class Student
+    {
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        [Key]
+        public int RollNo { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public int Math { get; set; }
+        
+        [Required]
+        public int Physics { get; set; }
+        
+        [Required]
+        public int Chemistry { get; set; }
+        public float Average { get => (Math + Physics + Chemistry) / 3; }
+    }
+}
